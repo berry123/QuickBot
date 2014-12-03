aaeff61f6da7fe6f84325cc174e7788d27e7e0c0
vaddera <vaddera@rose-hulman.edu>
2014-12-03 18:50:45 -0500

    Code finalized

--- Lab0_Alex/Demo/demoLab1/demoLab1/demoLab1.c
+++ Lab0_Alex/Demo/demoLab1/demoLab1/demoLab1.c
@@ -9,41 +9,116 @@
 #include <avr/io.h>
 #include "capi324v221.h"
 
-//void square(void);
-//void circle(void);
-//void eight(void);
+void moveSquare(void);
+void moveCircle(void);
+void moveEight(void);
 
 void CBOT_main(void)
 {
+	//unsigned char flag = 0;
 	LCD_open(); // Opening LCD Subsystem management.
+	LCD_printf("Setting up...");
 	
 	SUBSYS_OPENSTAT openTiny; // Here you can add multiple variables as you open subsystems.
 	
 	openTiny = ATTINY_open();
 	
 	if (openTiny.state != SUBSYS_OPEN){ // state comes from the struct SUBSYS_OPENSTAT_TYPE
+		LCD_clear();
 		LCD_printf("Error when opening ATTINY!");
 	}
+	LCD_printf("\nReady!");
+	
 	
     while(1)
     {
-        if (ATTINY_get_SW_state(ATTINY_SW3)){
+		/*if (flag==0){
+			LCD_clear();
+			flag = 1;
+		}
+		
+		LCD_printf("SW3 - Square path.\nSW4 - Circle path.\nSW5 - Eight path.");*/
+        
+		if (ATTINY_get_SW_state(ATTINY_SW3)){
+			LCD_clear();
 			LCD_printf("Square path.");
-			//square();
+			moveSquare();
+			LCD_clear();
 		}
 		else if (ATTINY_get_SW_state(ATTINY_SW4)){
+			LCD_clear();
 			LCD_printf("Circle path.");
-			//circle();
+			moveCircle();
+			LCD_clear();
 		}
 		else if (ATTINY_get_SW_state(ATTINY_SW5)){
+			LCD_clear();
 			LCD_printf("Eight path.");
-			//eight();
+			moveEight();
+			LCD_clear();
 		}
     }
 }
 
-/*void square(){
+void moveSquare(void){
+	unsigned char i;
+	SUBSYS_OPENSTAT openStepper;
 	
+	openStepper = STEPPER_open();
 	
+	if (openStepper.state != SUBSYS_OPEN){
+		LCD_printf("\nError when opening STEPPER subsystem!");
+	}
 	
-}*/
+	for (i=0;i<4;i++){ // Runs the same routine twice in order to make the robot move in a square fashion.
+		// Moving both wheels forward.
+		STEPPER_move_stwt(STEPPER_BOTH,
+			STEPPER_FWD, 570, 200, 400, STEPPER_BRK_OFF,	//Left wheel
+			STEPPER_FWD, 570, 200, 400, STEPPER_BRK_OFF);	//Right wheel
+		
+		// Turning robot to the right.
+		STEPPER_move_stwt(STEPPER_BOTH,
+			STEPPER_FWD, 120, 200, 400, STEPPER_BRK_OFF,	//Left wheel
+			STEPPER_REV, 120, 200, 400, STEPPER_BRK_OFF);	//Right wheel
+	}
+	
+	STEPPER_close();
+}
+
+void moveCircle(void){
+	SUBSYS_OPENSTAT openStepper;
+	
+	openStepper = STEPPER_open();
+	
+	if (openStepper.state != SUBSYS_OPEN){
+		LCD_printf("\nError when opening STEPPER subsystem!");
+	}
+	
+	STEPPER_move_stwt(STEPPER_BOTH,
+		STEPPER_FWD, 2050, 205, 400, STEPPER_BRK_OFF,		//Left wheel
+		STEPPER_FWD, 1100, 110, 400, STEPPER_BRK_OFF);	//Right wheel
+	
+	STEPPER_close();
+}
+
+void moveEight(void){
+	SUBSYS_OPENSTAT openStepper;
+	
+	openStepper = STEPPER_open();
+	
+	if (openStepper.state != SUBSYS_OPEN){
+		LCD_printf("\nError when opening STEPPER subsystem!");
+	}
+	
+	STEPPER_move_stwt(STEPPER_BOTH,
+		STEPPER_FWD, 2050, 205, 400, STEPPER_BRK_OFF,		//Left wheel
+		STEPPER_FWD, 1100, 110, 400, STEPPER_BRK_OFF);	//Right wheel
+		
+	TMRSRVC_delay(2000);
+	
+	STEPPER_move_stwt(STEPPER_BOTH,
+		STEPPER_FWD, 1100, 110, 400, STEPPER_BRK_OFF,		//Left wheel
+		STEPPER_FWD, 2050, 205, 400, STEPPER_BRK_OFF);	//Right wheel
+		
+	STEPPER_close();
+}
