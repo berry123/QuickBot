91e3abcd734e51dc4a0c21c21be2860bfd8c0cdd
vaddera <vaddera@rose-hulman.edu>
2014-12-08 14:34:39 -0500

    stuff to be committed

--- Lab0_Alex/MATLAB/lab1.m
+++ Lab0_Alex/MATLAB/lab1.m
@@ -4,7 +4,7 @@ close all
 
 %% Variables:
 D = 3.375;
-CWxoff = [2.5, -0.05, -2, 3, 0];
+CWxoff = [2.5, -0.5, -2, 3, 0];
 CWyoff = [3.5, 4.5, 4, 3.5, 3.5];
 CCWxoff = [-2.5, -4.5, -4, -4.5, -4];
 CCWyoff = [-1.5, -1.5, -0.5, -2, -2];
@@ -14,14 +14,14 @@ CWx = mean(CWxoff);
 CWy = mean(CWyoff);
 CCWx = mean(CCWxoff);
 CCWy = mean(CCWyoff);
-rCW = sqrt(CWx.^2 + CWy.^2);
-rCCW = sqrt(CCWx.^2 + CCWy.^2);
+rCW = sqrt(CWx^2 + CWy^2);
+rCCW = sqrt(CCWx^2 + CCWy^2);
 EMaxSys = max([rCW,rCCW]);
 
 alpha = mean([(CWx+CCWx)/(-4*D),(CWy-CCWy)/(-4*D)]);
 beta = mean([(CWx-CCWx)/(-4*D),(CWy+CCWy)/(-4*D)]);
 
-Ed = (D+beta*sin(beta/2))/(D-beta*sin(beta/2));
+Ed = (D+B*sin(beta/2))/(D-B*sin(beta/2));
 cb = pi/(pi-alpha)
 cl = 2/(Ed+1)
 cr = Ed*cl
