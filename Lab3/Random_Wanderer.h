e55a3794d130f89a1ef28e2da64134eb01e248c3
ValRose <olejnikpr@gmail.com>
2014-12-16 23:12:55 -0500

    added Random Wanderer

---
+++ Lab3/Random_Wanderer.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Random_Wanderer(){
+	char RightWheelSpeed;
+	char LeftWheelSpeed;
+	while(1){
+		// Randomly select speed of wheels
+		RightWheelSpeed = rand() % 201;
+		LeftWheelSpeed = rand() % 201;
+
+		STEPPER_move_rn(STEPPER_BOTH,
+			STEPPER_FWD, LeftWheelSpeed, 400,	//Left
+			STEPPER_FWD, RightWheelSpeed, 400);	//Right
+		TMRSRVC_delay(500); //5 Sec duration
+		}
+	}
