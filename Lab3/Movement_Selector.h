650465e70fa10eabecbb7eded38d46f999641d7d
ValRose <olejnikpr@gmail.com>
2014-12-16 21:01:05 -0500

    Movement_Sellector made

--- Lab3/Movement_Selector.h
+++ Lab3/Movement_Selector.h
@@ -8,20 +8,58 @@ void Movement_Selctor_Excecutor(signed char FSS, signed char BSS, signed char RS
 */
 
 	switch(TS){
-		case 0:
-
-			break;
 		case 1:
-
+			if ((FSS == 1) && (BSS == 0) && (RSS == 0) &&(LSS == 0)){
+				Backward_Move();
+				}
+			else if ((FSS == 0) && (BSS == 1) && (RSS == 0) &&(LSS == 0)){
+				Forward_Move();
+				}
+			else if ((FSS == 0) && (BSS == 0) && (RSS == 0) &&(LSS == 1)){
+				Soft_Forward_Right();
+				}
+			else {
+				Soft_Forward_Left();
+				}
 			break;
 		case 2:
-
+			if ((FSS == 1) && (BSS == 0) && (RSS == 0) &&(LSS == 1)){
+				Soft_Backward_Right();
+				}
+			else if ((FSS == 1) && (BSS == 0) && (RSS == 1) &&(LSS == 0)){
+				Soft_Backward_Left();;
+				}
+			else if ((FSS == 0) && (BSS == 1) && (RSS == 0) &&(LSS == 1)){
+				Soft_Forward_Right();
+				}
+			else if ((FSS == 0) && (BSS == 1) && (RSS == 1) &&(LSS == 0)){
+				Soft_Forward_Left();
+				}
+			else {
+				Forward_Move();
+				}
+			
 			break;
 		case 3:
-
+			if ((FSS == 1) && (BSS == 1) && (RSS == 0) &&(LSS == 1)){
+				Hard_Right();
+				}
+			else if ((FSS == 1) && (BSS == 1) && (RSS == 1) &&(LSS == 0)){
+				Hard_Left();
+				}
+			else if ((FSS == 1) && (BSS == 0) && (RSS == 1) &&(LSS == 1)){
+				Backward_Move();
+				}
+			else {
+				Forward_Move();
+				}
+			
+			break;
+		case 4:
+			Cry();
 			break;
 		default:
-
+			// just stay and chill!
 			break;
 	}
 }
