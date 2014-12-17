4571e6ad86d5547922e4e70988410a9011655bb8
ValRose <olejnikpr@gmail.com>
2014-12-17 18:45:24 -0500

    updates

--- Lab3/Random_Wanderer.h
+++ Lab3/Random_Wanderer.h
@@ -15,3 +15,19 @@ void Random_Wanderer(){
 		TMRSRVC_delay(500); //5 Sec duration
 		}
 	}
+
+void Shy_Random_Wanderer(){
+	char RightWheelSpeed;
+	char LeftWheelSpeed;
+	while(1){
+		// Randomly select speed of wheels
+		RightWheelSpeed = rand() % 201;
+		LeftWheelSpeed = rand() % 201;
+
+		STEPPER_move_rn(STEPPER_BOTH,
+			STEPPER_FWD, LeftWheelSpeed, 400,	//Left
+			STEPPER_FWD, RightWheelSpeed, 400);	//Right
+		TMRSRVC_delay(500); //5 Sec duration
+		shy();
+		}
+	}
