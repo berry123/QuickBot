543130fd00899d8fe4033fb2d07b3e88ac7171bc
vaddera <vaddera@rose-hulman.edu>
2014-12-17 19:06:09 -0500

    updated

--- Lab3/lab3demo/lab3demo/Random_Wanderer.h
+++ Lab3/lab3demo/lab3demo/Random_Wanderer.h
@@ -4,14 +4,30 @@
 void Random_Wanderer(){
 	char RightWheelSpeed;
 	char LeftWheelSpeed;
-	while(1){
+	//while(1){
 		// Randomly select speed of wheels
-		RightWheelSpeed = rand() % 201;
-		LeftWheelSpeed = rand() % 201;
+	RightWheelSpeed = rand() % 201;
+	LeftWheelSpeed = rand() % 201;
 
-		STEPPER_move_rn(STEPPER_BOTH,
-			STEPPER_FWD, LeftWheelSpeed, 400,	//Left
-			STEPPER_FWD, RightWheelSpeed, 400);	//Right
-		TMRSRVC_delay(500); //5 Sec duration
-		}
+	STEPPER_move_rn(STEPPER_BOTH,
+		STEPPER_FWD, LeftWheelSpeed, 400,	//Left
+		STEPPER_FWD, RightWheelSpeed, 400);	//Right
+	TMRSRVC_delay(500); //5 Sec duration
+		//}
 	}
+
+void Shy_Random_Wanderer(){
+	char RightWheelSpeed;
+	char LeftWheelSpeed;
+	//while(1){
+	// Randomly select speed of wheels
+	RightWheelSpeed = rand() % 201;
+	LeftWheelSpeed = rand() % 201;
+
+	STEPPER_move_rn(STEPPER_BOTH,
+		STEPPER_FWD, LeftWheelSpeed, 400,	//Left
+		STEPPER_FWD, RightWheelSpeed, 400);	//Right
+	TMRSRVC_delay(500); //5 Sec duration
+	shy();
+		//}
+}
