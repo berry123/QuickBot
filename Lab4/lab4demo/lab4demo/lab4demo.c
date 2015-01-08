65d1a368ac56b159b949c03b13f1ed6231e65e17
ValRose <olejnikpr@gmail.com>
2015-01-07 21:54:39 -0500

    stuff

--- Lab4/lab4demo/lab4demo/lab4demo.c
+++ Lab4/lab4demo/lab4demo/lab4demo.c
@@ -84,12 +84,6 @@ void CBOT_main(void)
 						STEPPER_FWD, 150, 400);	//Right
 					TMRSRVC_delay(500);
 					if (leftIR > rightIR){
-<<<<<<< HEAD
-						go2Angle(-90);
-						leftFollow();
-					} else {
-						go2Angle(90);
-=======
 						STEPPER_move_rn(STEPPER_BOTH,
 							STEPPER_REV, 200, 400,	//Left
 							STEPPER_FWD, 200, 400);	//Right
@@ -100,7 +94,6 @@ void CBOT_main(void)
 							STEPPER_FWD, 200, 400,	//Left
 							STEPPER_REV, 200, 400);	//Right
 						TMRSRVC_delay(500);
->>>>>>> origin/master
 						rightFollow();
 					}
 				} else {
