f8b7fd9acead6cad373b514fbf3fa3df656747c6
vaddera <vaddera@rose-hulman.edu>
2015-01-08 00:59:11 -0500

    to commit

--- Lab4/lab4demo/lab4demo/goToPoint.h
+++ Lab4/lab4demo/lab4demo/goToPoint.h
@@ -190,4 +190,5 @@ void go2ContAngle(char angle, int speed){
 		STEPPER_FWD, speed, 400,	//Left
 		STEPPER_REV, speed, 400);	//Right
 	TMRSRVC_delay(500);
+	STEPPER_stop(STEPPER_BOTH, STEPPER_BRK_OFF);
 }
