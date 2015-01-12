befa4036ab5a66f18af85c091cbc8babf85fe951
vaddera <vaddera@rose-hulman.edu>
2015-01-11 20:48:41 -0500

    to commit

--- Lab4/lab4demo/lab4demo/wallFollow2.h
+++ Lab4/lab4demo/lab4demo/wallFollow2.h
@@ -363,8 +363,8 @@ void rightStepFollow(void){
 
 			// Forward Movement
 			STEPPER_move_stwt(STEPPER_BOTH,
-				STEPPER_FWD, 200, 200, 400, STEPPER_BRK_OFF,	//Left wheel
-				STEPPER_FWD, 200, 200, 400, STEPPER_BRK_OFF);	// Right wheel
+				STEPPER_FWD, 200, LWS, 400, STEPPER_BRK_OFF,	//Left wheel
+				STEPPER_FWD, 200, RWS, 400, STEPPER_BRK_OFF);	// Right wheel
 
 			oLWS = LWS;
 			oRWS = RWS;
@@ -475,9 +475,9 @@ void leftStepFollow(void){
 			RWS = RWS + KD*(oRWS-RWS);
 
 			// Forward Movement
-			STEPPER_move_rn(STEPPER_BOTH,
-					STEPPER_FWD, LWS, 400,	//Left
-					STEPPER_FWD, RWS, 400);	//Right
+			STEPPER_move_stwt(STEPPER_BOTH,
+					STEPPER_FWD, 200, LWS, 400, STEPPER_BRK_OFF,	//Left
+					STEPPER_FWD, 200, RWS, 400, STEPPER_BRK_OFF);	//Right
 
 			oLWS = LWS;
 			oRWS = RWS;
@@ -503,16 +503,16 @@ void leftStepFollow(void){
 			LCD_printf("Lack of Wall\n");
 			LCD_printf("Turning Left\n");
 
-			STEPPER_move_rn(STEPPER_BOTH,
-					STEPPER_FWD, 150, 400,	//Left
-					STEPPER_FWD, 150, 400);	//Right
+			STEPPER_move_stwt(STEPPER_BOTH,
+					STEPPER_FWD, 200, 150, 400, STEPPER_BRK_OFF,	//Left
+					STEPPER_FWD, 200, 150, 400, STEPPER_BRK_OFF);	//Right
 			TMRSRVC_delay(500);
 			leftIR = getLeftIR();
 			if ((frontIR > ICT) && (leftIR > ICT)){
-				go2ContAngle(-90, 100);
-				STEPPER_move_rn(STEPPER_BOTH,
-						STEPPER_FWD, 150, 400,	//Left
-						STEPPER_FWD, 150, 400);	//Right
+				go2ContAngle(90);
+				STEPPER_move_stwt(STEPPER_BOTH,
+						STEPPER_FWD, 200, 150, 400, STEPPER_BRK_OFF,	//Left
+						STEPPER_FWD, 200, 150, 400, STEPPER_BRK_OFF);	//Right
 				leftIR = getLeftIR();
 				while(leftIR > ICT){
 					TMRSRVC_delay(100);
@@ -525,4 +525,76 @@ void leftStepFollow(void){
 			}
 		}
 	}
+}
+
+void centerStepFollow(void){
+	char tollerance_band = 0;
+	float LP, RP, LWS, RWS, frontIR, rightIR, leftIR, avrageIR;
+	float oLWS = 0;
+	float oRWS = 0;
+	float LI = 0;
+	float RI = 0;
+	char base = 120;
+	char KP = 3;
+	char KI = 0.85;
+	char ICT = 12;
+	char KD = 0;
+
+	rightIR = getRightIR();
+	leftIR = getLeftIR();
+	while ((leftIR < ICT) && (rightIR < ICT)){
+		rightIR = getRightIR();
+		leftIR = getLeftIR();
+		avrageIR = (rightIR + leftIR)/2;
+		if ((rightIR - avrageIR) > tollerance_band){
+			//means left of center
+			LCD_clear();
+			LCD_printf("Center Follow\n");
+			LCD_printf("Left of\n");
+			LCD_printf("Center\n");
+
+			LP = rightIR - avrageIR;
+			RP = 0;
+			LI = LI++;
+			RI = 0;
+		} else if ((avrageIR - rightIR) > (tollerance_band)) {
+			//means right of center
+			LCD_clear();
+			LCD_printf("Center Follow\n");
+			LCD_printf("Right of\n");
+			LCD_printf("Center\n");
+
+			LP = 0;
+			RP = leftIR - avrageIR;
+			LI = 0;
+			RI = RI++;
+		} else {
+			//means center of center
+			LCD_clear();
+			LCD_printf("I shall\n");
+			LCD_printf("fear no\n");
+			LCD_printf("evil\n");
+
+			LP = 0;
+			RP = 0;
+			LI = 0;
+			RI = 0;
+		}
+		LWS = KP*LP + KI*LI + KD*oLWS + base;
+		RWS = KP*RP + KI*RI + KD*oRWS + base;
+		if (oLWS == 0){
+			oLWS = LWS;
+			oRWS = RWS;
+		}
+		LWS = LWS + KD*(oLWS-LWS);
+		RWS = RWS + KD*(oRWS-RWS);
+
+		// Forward Movement
+		STEPPER_move_stwt(STEPPER_BOTH,
+				STEPPER_FWD, 200, LWS, 400, STEPPER_BRK_OFF	//Left
+				STEPPER_FWD, 200, RWS, 400, STEPPER_BRK_OFF);	//Right
+
+		oLWS = LWS;
+		oRWS = RWS;
+	}	
 }
