200b1d23d0d4df7d7d6377c4afde362601b1fe11
vaddera <vaddera@rose-hulman.edu>
2015-01-11 20:30:19 -0500

    to commit

--- Lab4/lab4demo/lab4demo/wallFollow2.h
+++ Lab4/lab4demo/lab4demo/wallFollow2.h
@@ -413,3 +413,116 @@ void rightStepFollow(void){
 		}
 	}
 }
+
+void leftFollow(void){
+	char lowerLimit = 4.75;
+	char higherLimit = 5.25;
+	char ICT = 12;
+	float LP, RP, LWS, RWS, frontIR, rightIR, leftIR;
+	float oLWS = 0;
+	float oRWS = 0;
+	float LI = 0;
+	float RI = 0;
+	char base = 120;
+	char KP = 3;
+	char KI = 0.75;
+	char KD = 0;
+
+	while(1){
+		frontIR = getFrontIR();
+		rightIR = getRightIR();
+		leftIR = getLeftIR();
+		if((frontIR > ICT) && (leftIR < ICT)){
+			if (leftIR < lowerLimit){
+				LCD_clear();
+				LCD_printf("Left Follow\n");
+				LCD_printf("Inside Target\n");
+				LCD_printf("Range\n");
+
+				LP = lowerLimit - leftIR;
+				RP = 0;
+				LI = LI++;
+				RI = 0;
+			}else if(leftIR > higherLimit){
+				LCD_clear();
+				LCD_printf("Left Follow\n");
+				LCD_printf("Outside Target\n");
+				LCD_printf("Range\n");
+
+				LP = 0;
+				RP = leftIR - higherLimit;
+				LI = 0;
+				RI = RI++;
+			}else{
+				LCD_clear();
+				LCD_printf("Left Follow\n");
+				LCD_printf("At Target\n");
+				LCD_printf("Range\n");
+
+				LP = 0;
+				RP = 0;
+				LI = 0;
+				RI = 0;
+			}
+
+			LWS = KP*LP + KI*LI + base;
+			RWS = KP*RP + KI*RI + base;
+			if (oLWS == 0){
+				oLWS = LWS;
+				oRWS = RWS;
+			}
+			LWS = LWS + KD*(oLWS-LWS);
+			RWS = RWS + KD*(oRWS-RWS);
+
+			// Forward Movement
+			STEPPER_move_rn(STEPPER_BOTH,
+					STEPPER_FWD, LWS, 400,	//Left
+					STEPPER_FWD, RWS, 400);	//Right
+
+			oLWS = LWS;
+			oRWS = RWS;
+
+		}else if((frontIR < ICT) && (leftIR < ICT)){
+			LCD_clear();
+			LCD_printf("Left Follow\n");
+			LCD_printf("Wall in Front\n");
+			LCD_printf("Turning Right\n");
+
+			STEPPER_move_rn(STEPPER_BOTH,
+					STEPPER_FWD, 150, 400,	//Left
+					STEPPER_FWD, 150, 400);	//Right
+			TMRSRVC_delay(500);
+			go2ContAngle(90, 100);
+			LP = 0;
+			RP = 0;
+			LI = 0;
+			RI = 0;	
+		}else if((frontIR > ICT) && (leftIR > ICT)){
+			LCD_clear();
+			LCD_printf("Left Follow\n");
+			LCD_printf("Lack of Wall\n");
+			LCD_printf("Turning Left\n");
+
+			STEPPER_move_rn(STEPPER_BOTH,
+					STEPPER_FWD, 150, 400,	//Left
+					STEPPER_FWD, 150, 400);	//Right
+			TMRSRVC_delay(500);
+			leftIR = getLeftIR();
+			if ((frontIR > ICT) && (leftIR > ICT)){
+				go2ContAngle(-90, 100);
+				STEPPER_move_rn(STEPPER_BOTH,
+						STEPPER_FWD, 150, 400,	//Left
+						STEPPER_FWD, 150, 400);	//Right
+				leftIR = getLeftIR();
+				while(leftIR > ICT){
+					TMRSRVC_delay(100);
+					leftIR = getLeftIR();
+				}
+				LP = 0;
+				RP = 0;
+				LI = 0;
+				RI = 0;	
+			}
+		}
+	}
+}
