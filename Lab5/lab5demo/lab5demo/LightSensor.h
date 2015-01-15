4d8b02f97ad4ec88cc7bdd32597728a4983a4854
vaddera <vaddera@rose-hulman.edu>
2015-01-14 21:44:13 -0500

    updated

--- Lab5/lab5demo/lab5demo/LightSensor.h
+++ Lab5/lab5demo/lab5demo/LightSensor.h
@@ -31,32 +31,3 @@ float getRightLight(){
 	//return percent;
 	return voltage*20;
 }
-=======
-float getLeftLight(void){
-	float Light_Percentage, V_in;
-	
-	//Sensor stuff
-	
-	Light_Percentage = Light_Calculator(V_in);
-	
-	return Light_Percentage;
-}
-
-float getRightLight(void){
-	float Light_Percentage, V_in;
-		
-		//Sensor stuff
-		
-		Light_Percentage = Light_Calculator(V_in);
-		
-		return Light_Percentage;	
-}
-
-float Light_Calculator(float V_in){
-	float Light_Percentage;
-	
-	Light_Percentage = V_in*20;//(V_in-1.667)*60;
-	
-	return Light_Percentage;
-}
->>>>>>> origin/master
