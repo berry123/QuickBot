4e3d8415074f4289ca0c4d569d3658db45ed21bf
ValRose <olejnikpr@gmail.com>
2015-01-14 22:32:56 -0500

    ALC added

--- Lab5/lab5demo/lab5demo/LightActions.h
+++ Lab5/lab5demo/lab5demo/LightActions.h
@@ -1,15 +1,16 @@
 void light_fear(void){
-	float Left_Light, Right_Light;
+	float Left_Light, Right_Light, Callibration;
 	int basespeed = 250;
 
 	LCD_clear();
 	LCD_printf("ARGG!!\n");
 	LCD_printf("IT BURNS!!\n");
 	LCD_printf("IT BURNS US!!\n");
-
+	Callibration = ALC();
+	
 	while(1){
-		Left_Light = getLeftLight();
-		Right_Light = getRightLight();
+		Left_Light = getLeftLight(Callibration);
+		Right_Light = getRightLight(Callibration);
 		STEPPER_move_rn(STEPPER_BOTH,
 				STEPPER_BCK, (Right_Light*basespeed/100), 400,	//Left
 				STEPPER_BCK, (Left_Light*basespeed/100), 400);	//Right
@@ -18,7 +19,7 @@ void light_fear(void){
 }
 
 void light_aggression(void){
-	float Left_Light, Right_Light;
+	float Left_Light, Right_Light, Callibration;
 	int basespeed = 250;
 
 	LCD_clear();
@@ -26,10 +27,11 @@ void light_aggression(void){
 	LCD_printf("AAAAAAA...\n");
 	LCD_printf("AAAAAAA...\n");
 	LCD_printf("AAAWWR\n");
+	Callibration = ALC();
 
 	while(1){
-		Left_Light = getLeftLight();
-		Right_Light = getRightLight();
+		Left_Light = getLeftLight(Callibration);
+		Right_Light = getRightLight(Callibration);
 		STEPPER_move_rn(STEPPER_BOTH,
 				STEPPER_FWD, (Left_Light*basespeed/100), 400,	//Left
 				STEPPER_FWD, (Right_Light*basespeed/100), 400);	//Right
@@ -38,16 +40,17 @@ void light_aggression(void){
 }
 
 void light_love(void){
-	float Left_Light, Right_Light;
+	float Left_Light, Right_Light, Callibration;
 	int basespeed = 250;
 
 	LCD_clear();
 	LCD_printf("mmmmmmm...\n");
 	LCD_printf("light...\n");
+	Callibration = ALC();
 
 	while(1){
-		Left_Light = getLeftLight();
-		Right_Light = getRightLight();
+		Left_Light = getLeftLight(Callibration);
+		Right_Light = getRightLight(Callibration);
 		STEPPER_move_rn(STEPPER_BOTH,
 				STEPPER_FWD, ((100-Left_Light)*basespeed/100), 400,	//Left
 				STEPPER_FWD, ((100-Right_Light)*basespeed/100), 400);	//Right
@@ -56,16 +59,17 @@ void light_love(void){
 }
 
 void light_explorer(void){
-	float Left_Light, Right_Light;
+	float Left_Light, Right_Light, Callibration;
 	int basespeed = 250;
 
 	LCD_clear();
 	LCD_printf("Ola!\n");
 	LCD_printf("Me lamo Dora!\n");
+	Callibration = ALC();
 
 	while(1){
-		Left_Light = getLeftLight();
-		Right_Light = getRightLight();
+		Left_Light = getLeftLight(Callibration);
+		Right_Light = getRightLight(Callibration);
 		STEPPER_move_rn(STEPPER_BOTH,
 				STEPPER_FWD, ((100-Right_Light)*basespeed/100), 400,	//Left
 				STEPPER_FWD, ((100-Left_Light)*basespeed/100), 400);	//Right
