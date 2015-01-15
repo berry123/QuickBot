a00a90f3d7d261a99425569724bcda50ca005b72
vaddera <vaddera@rose-hulman.edu>
2015-01-14 21:42:52 -0500

    updates

--- Lab5/lab5demo/lab5demo/LightSensor.h
+++ Lab5/lab5demo/lab5demo/LightSensor.h
@@ -1,27 +1,32 @@
-float getLeftLight(void){
-	float Light_Percentage, V_in;
-	
-	//Sensor stuff
-	
-	Light_Percentage = Light_Calculator(V_in)
-	
-	return Light_Percentage;
+#define RIGHT_PHOTO ADC_CHAN5
+#define LEFT_PHOTO ADC_CHAN6
+
+float getLeftLight(){
+
+	float voltage, percent;
+	ADC_SAMPLE adcsample;
+	ADC_set_VREF( ADC_VREF_AVCC );
+	ADC_set_channel( LEFT_PHOTO );
+	adcsample = ADC_sample();
+	//LCD_printf( "ADC: %i\n",adcsample);
+	voltage = adcsample * (5.0/ 1024.0 );
+	//LCD_printf( "voltage: %f\n",voltage);
+	//percent = (voltage-1.67)*60;
+	//return percent;
+	return voltage*20;
 }
 
-float getRightLight(void){
-	float Light_Percentage, V_in;
-		
-		//Sensor stuff
-		
-		Light_Percentage = Light_Calculator(V_in)
-		
-		return Light_Percentage;	
-}
-
-float Light_Calculator(float V_in){
-	float Light_Percentage;
-	
-	Light_Percentage = (V_in-1.667)*60
-	
-	return Light_Percentage;
+// getRightLight() converts ADC voltage to value inverse proportional to left light
+float getRightLight(){
+	ADC_SAMPLE adcsample;
+	float voltage, percent;
+	ADC_set_VREF( ADC_VREF_AVCC );
+	ADC_set_channel( RIGHT_PHOTO );
+	adcsample = ADC_sample();
+	//LCD_printf( "ADC: %i\n",adcsample);
+	voltage = adcsample * ( 5.0 / 1024 );
+	//LCD_printf( "right: %f\n",voltage);
+	//percent = (voltage-1.67)*60;
+	//return percent;
+	return voltage*20;
 }
--- Lab5/lab5demo/lab5demo/lab5demo.c
+++ Lab5/lab5demo/lab5demo/lab5demo.c
@@ -10,15 +10,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include "capi324v221.h"
-#include "Capek_Movements.h"	// Free Running functions for each type of movement.
-#include "Movement_Selector.h"	// Sensor reading logic function.
-#include "Random_Wanderer.h"	// Where both the random wanderer and the random wanderer that is affected by sensor readings are defined.
+//#include "Capek_Movements.h"	// Free Running functions for each type of movement.
+#include "LightSensor.h"
+//#include "Movement_Selector.h"	// Sensor reading logic function.
+//#include "Random_Wanderer.h"	// Where both the random wanderer and the random wanderer that is affected by sensor readings are defined.
 #include "IRlib.h"				// IR sensor reading functions.
-#include "goToPoint.h"			// Go to point functions.
-
-// Used function prototypes.
-void aggressive();
-void shy();
+//#include "goToPoint.h"			// Go to point functions.
 
 // Main function
 void CBOT_main(void)
@@ -26,6 +23,7 @@ void CBOT_main(void)
 	// local main variables:
 	signed char cox = 0;
 	signed char coy = 0;
+	float rightLight, leftLight;
 	
 	// Opening LCD subsystem management.
 	LCD_open();
@@ -66,98 +64,25 @@ void CBOT_main(void)
 		
 		// Press SW3 to select one of the kids.
 		if (ATTINY_get_SW_state(ATTINY_SW3)){
-			while(!ATTINY_get_SW_state(ATTINY_SW5)){ // SW5 returns to main menu
+			
+				rightLight = getRightLight();
+				leftLight = getLeftLight();
+			
 				LCD_clear();
-				LCD_printf("SW3: Aggressive");
-				LCD_printf("\nSW4: Shy");
-				LCD_printf("\nSW5: Back");
-				if(ATTINY_get_SW_state(ATTINY_SW3)){
-					while(1){
-						// Selects the aggressive kid
-						aggressive();
-					}
-				}else if(ATTINY_get_SW_state(ATTINY_SW4)){
-					while(1){
-						// Selects the shy kid
-						shy();
-					}
-				}
-			}
+				LCD_printf("%f\n%f", rightLight, leftLight);
+				TMRSRVC_delay(2000);
+				
 		}else if(ATTINY_get_SW_state(ATTINY_SW4)){
-			LCD_clear();
-			LCD_printf("SW3: Random.");
-			LCD_printf("\nSW4: Subsumption.");
-			LCD_printf("\nSW5: Back");
-			while(!ATTINY_get_SW_state(ATTINY_SW5)){ // SW5 returns to main menu
-				if(ATTINY_get_SW_state(ATTINY_SW3)){
-					while(1){
-						// Selects the random wanderer
-						Random_Wanderer();
-					}
-				}else if(ATTINY_get_SW_state(ATTINY_SW4)){
-					while(1){
-						// Selects Subsumption
-						Shy_Random_Wanderer();
-					}
-				}
-			}
+			//
 		}else if(ATTINY_get_SW_state(ATTINY_SW5)){
-			// While SW5 is not pressed...
-			while(!ATTINY_get_SW_state(ATTINY_SW5)){
-				LCD_clear();
-				LCD_printf("( %d , %d )", cox, coy); // Displays the angle selection.
-				LCD_printf("\nSW3 + | SW4 - \nSW5 To y");
-				// If SW3 is pressed, the coordinate x is incremented.
-				if(ATTINY_get_SW_state(ATTINY_SW3)){
-					cox++;
-					// If the x value is > 5, it goes back to -5.
-					if (cox > 5){
-						cox = -5;
-					}
-					// If SW4 is pressed, the coordinate x is decremented.
-					}else if(ATTINY_get_SW_state(ATTINY_SW4)){
-					cox--;
-					// If the x value is < -5, it goes back to 5.
-					if(cox < -5){
-						cox = 5;
-					}
-				}
-			}
-			// After choosing the value of the coordinate x, the user must choose the value of the coordinate y:
-			// While SW5 is not pressed...
-			while(!ATTINY_get_SW_state(ATTINY_SW5)){
-				LCD_clear();
-				LCD_printf("( %d , %d )", cox, coy);
-				LCD_printf("\nSW3 + | SW4 - \nSW5 Enter");
-				// If SW3 is pressed, the coordinate y is incremented.
-				if(ATTINY_get_SW_state(ATTINY_SW3)){
-					coy++;
-					// If the x value is > 5, it goes back to -5.
-					if (coy > 5){
-						coy = -5;
-					}
-					// If SW4 is pressed, the coordinate y is decremented.
-					}else if(ATTINY_get_SW_state(ATTINY_SW4)){
-						coy--;
-					// If the x value is < -5, it goes back to 5
-					if(coy < -5){
-						coy = 5;
-					}
-				}
-			}
-			// Calls go to goal point function.
-			go2Point(cox,coy);
-			// Clears the variables up.
-			cox = 0;
-			coy = 0;
-			LCD_clear();
+			//
 		}
 		// Keeps running.
     }
 }
 
 
-// Function for the aggressive kid.
+/*// Function for the aggressive kid.
 void aggressive(){
 	
 	float frontIR = 0;
@@ -216,5 +141,5 @@ void shy(){
 	Movement_Selector_Excecutor(FSS, BSS, RSS, LSS, TS);
 }
 
-
+*/
 
